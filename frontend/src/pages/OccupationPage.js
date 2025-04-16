e8097e82ac8ad6507fa724bbfc6ef7594b35da08
Florentin <florentin.corbiat@yahoo.fr>
2025-04-16 17:48:06 +0200

    accés occupation + bg map siege ok

--- frontend/src/pages/OccupationPage.js
+++ frontend/src/pages/OccupationPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useUser } from '../UserContext';
+import '../assets/style/OccupationPage.css';
 
 const OccupationPage = () => {
   const navigate = useNavigate();
@@ -8,20 +9,12 @@ const OccupationPage = () => {
 
   useEffect(() => {
     if (!user) {
-      navigate('/Register');
+      navigate('/register');
     }
   }, [user, navigate]);
 
   return (
-    <div>
-      <div className="page-container">
-        <h1>Occupation</h1>
-        <p>
-          Bienvenue sur la page Occupation ! Ici, vous pouvez trouver toutes les informations
-          pertinentes concernant les stratégies, les personnages et les équipements
-          recommandés pour progresser dans cette section du jeu.
-        </p>
-      </div>
+    <div className="occupation-container">
     </div>
   );
 };
